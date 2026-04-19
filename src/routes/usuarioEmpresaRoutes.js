84e6351929b2ac3a564c410bd50a9c39fd8b235e
Jose Bernal <josef.bernalc@konradlorenz.edu.co>
2026-04-19 14:37:22 -0500

    Correción router error nodemon

--- src/routes/usuarioEmpresaRoutes.js
+++ src/routes/usuarioEmpresaRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.router();
+const router = express.Router();
 const usuarioEmpresaSchema = require("/models/usuarioEmpresaModel.JS");
 
 router.post("/usuarioEmpresa" , (req, res)=>{
--- src/routes/usuarioVisitanteRoutes.js
+++ src/routes/usuarioVisitanteRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.router();
+const router = express.Routerouter();
 const usuarioVisitanteSchema = require("../models/usuarioVisitanteModel");
 
 router.post("/usuarioVisitante" , (req, res)=>{
