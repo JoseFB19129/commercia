c9d7a9dd63d6c40d8b6ab9f5f77c0a8884375649
Jose Bernal <josef.bernalc@konradlorenz.edu.co>
2026-04-19 14:33:38 -0500

    configuración inicial en el index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
+const parser = require('body-parser');
 const express = require('express')
 const app = express()
 const port = 3000
+const usuarioEmpresaRoutes = require("./routes/usuarioEmpresaRoutes");
+const usuarioVisitanteRoutes= require("./routes/usuarioVisitanteRoutes");
+const mongoose = require("mongoose");
+require('dotenv').config();
 
 app.get('/', (req, res)=>{
     res.send('Hola mundo')
 })
 
+app.use(parser.urlencoded({extended:false}));
+app.use(parse.json())
+
+app.use("/usuarioVisitante", usuarioVisitanteRoutes)
+app.use("/usuarioEmpresa", usuarioEmpresaRoutes);
+
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log("conexion exitosa"))
+    .catch((error) => console.log(error))
+    
 app.listen(port, () => {
      console.log('la app se está ejecutando en el puerto ' + `${port}`)
 })
