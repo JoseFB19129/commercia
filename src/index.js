abfd88a3cfc15962adc3cce3c168a010be5dee05
Jose Bernal <josef.bernalc@konradlorenz.edu.co>
2026-04-25 07:30:11 -0500

    actualizacion index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
 const parser = require('body-parser');
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 const usuarioEmpresaRoutes = require("./routes/usuarioEmpresaRoutes");
-const usuarioVisitanteRoutes= require("./routes/usuarioVisitanteRoutes");
+const usuarioVisitanteRoutes = require("./routes/usuarioVisitanteRoutes");
+const authRoutes = require("./routes/authRoutes");
 const mongoose = require("mongoose");
 require('dotenv').config();
 
-app.get('/', (req, res)=>{
-    res.send('Hola mundo')
-})
+app.get('/', (req, res) => {
+    res.send('API del Directorio de Negocios');
+});
 
-app.use(parser.urlencoded({extended:false}));
-app.use(parser.json())
+app.use(parser.urlencoded({ extended: false }));
+app.use(parser.json());
 
-app.use("/usuarioVisitante", usuarioVisitanteRoutes)
-app.use("/usuarioEmpresa", usuarioEmpresaRoutes);
+// Rutas
+app.use("/api/auth", authRoutes);
+app.use("/api/empresa", usuarioEmpresaRoutes);
+app.use("/api/visitante", usuarioVisitanteRoutes);
 
 mongoose
     .connect(process.env.MONGODB_URI)
-    .then(() => console.log("conexion exitosa"))
-    .catch((error) => console.log(error))
+    .then(() => console.log("Conexión exitosa a MongoDB"))
+    .catch((error) => console.log("Error de conexión:", error));
 
 app.listen(port, () => {
-     console.log('la app se está ejecutando en el puerto ' + `${port}`)
-})
+    console.log(`La app se está ejecutando en el puerto ${port}`);
+});
