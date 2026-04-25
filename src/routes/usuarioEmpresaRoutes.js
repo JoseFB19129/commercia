3224def0ad418fde6a17cd863d3c803b0464bb0a
Jose Bernal <josef.bernalc@konradlorenz.edu.co>
2026-04-25 07:26:59 -0500

    ajustes ruta de usuario empresa

--- src/routes/usuarioEmpresaRoutes.js
+++ src/routes/usuarioEmpresaRoutes.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const usuarioEmpresaSchema = require("../models/usuarioEmpresaModel");
+const { verifyToken, verifyEmpresaRole } = require("../middleware/auth");
+const { 
+    getProfile, 
+    updateProfile, 
+    changePassword, 
+    deleteAccount,
+    getAllEmpresas,
+    getEmpresaById
+} = require("../controllers/usuarioEmpresaController");
 
-router.post("/usuarioEmpresa" , (req, res)=>{
-    const usuarioEmpresa = usuarioEmpresaSchema(req.body);
-    usuarioVisitante
-        .save()
-        .then((data)=>res.json(data))
-        .catch((error)=> res.json({ message: error}));
-});
+// Rutas públicas (para consultar empresas)
+router.get("/empresas", getAllEmpresas);
+router.get("/empresas/:id", getEmpresaById);
 
-router.get("/usuarioEmpresa", (req, res)=> {
-    usuarioEmpresaSchema.find()
-        .then((data) => res.json(data))
-        .catch((error)=> res.json({message : error}))
-})
+// Rutas protegidas (solo para usuarios empresa autenticados)
+router.get("/perfil", verifyToken, verifyEmpresaRole, getProfile);
+router.put("/perfil", verifyToken, verifyEmpresaRole, updateProfile);
+router.put("/cambiar-contrasena", verifyToken, verifyEmpresaRole, changePassword);
+router.delete("/cuenta", verifyToken, verifyEmpresaRole, deleteAccount);
 
 module.exports = router;
